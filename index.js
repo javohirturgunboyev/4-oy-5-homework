422829ac4d287c769b93ac1a769df0e1ed04410b
javohirturgunboyev <javohirboy009@gmail.com>
2024-05-27 16:45:08 +0500

    tugadi

--- index.js
+++ index.js
@@ -64,3 +64,126 @@
 
 // 7-masalasi
 
+// let number =+prompt("son kiriting");
+
+// let sum =0;
+// let num = 0;
+
+// for(i=1; i<=number; i++ ){
+// if (number % i=== 0) {
+//     sum++;
+//     num+=i;
+// }
+// }
+
+// console.log(sum);
+// console.log(num);
+
+// 8-masalasi
+
+// let son = +prompt("son kiriting");
+// let result;
+// for(let i=2; i<son; i++ ){
+// if(i===0){
+// result=false;
+// }
+
+// if (i<=son) {
+//     result=false;
+
+// }
+// result=true;
+// }
+
+// const num = 123;
+// if (num) {
+ 
+   
+// }
+
+// console.log(`tub son ${son}`);
+// console.log(`tub emas ${num}`);
+
+// for masalalari
+
+// for1-masalasi
+
+// let k = +prompt("k sonini kiriting");
+// let n = +prompt("n sonini kiriting");
+
+// if (n<=0) {
+//     console.log(`0 n dan katta bo'lishi kerak`);
+// } 
+
+// let result=0;
+
+// for( let i=0; i < n; i++ ) {
+// result +=k;
+// }
+
+// console.log(`${k } ning ko'paytiring ${n} ga ${result}`);
+
+// for2-masalasi
+
+// let a = 2
+// let b = 4
+// for(a; a <= b; a++){
+//     console.log(a);
+// }
+
+
+
+// for-3 masalasi
+
+// let a = 2
+// let b = 5
+
+// for (let i = b - 1; i > a; i-- ) {
+//     console.log(i);
+    
+// } 
+
+
+
+// for-4masalasi
+
+// let kg = 10
+
+// for (let i = 1 ; i <= kg; i++ ) {
+//     console.log(i * kg);
+    
+// }
+
+// for-5 masalasi
+
+// const Kg = 1000; 
+
+// for (let i = 1; i <= 10; i++) {
+//     const son = i / 10;
+//     const result = son * Kg;
+//     console.log(${son} kg konfetning narxi: ${result} som);
+// }
+
+// for-6 masalasi
+
+// const kg = 1000;
+
+
+// for(let i = 10 ; i<= 20 ; i++) {
+//     const son = i / 10;
+//     const result =  son * kg 
+//     console.log( ${son} kg konfetning narxi: ${result} so'm );
+// }
+
+// for-7 masalasi
+// let a = 1
+// let b = 10
+// let result = 0 
+
+
+// for-8 masalasi
+//  (let i = a ; i <= b; i++ ) {
+//    result = result + i;
+    
+// }
+// console.log(result);
