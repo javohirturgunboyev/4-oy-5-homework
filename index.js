501304ebc754f758973dc63cf489a5f2c71cb8cd
javohirturgunboyev <javohirboy009@gmail.com>
2024-05-27 08:50:01 +0500

    tugadi

--- index.js
+++ index.js
@@ -2,9 +2,65 @@
 
 // 1-masalasi
 
-let n = +prompt("son kiriting")
+// let n = +prompt("son kiriting");
 
-for (let i=1 ; 1<=n ; i++) {
-    console.log(i);
+// for (let i=1 ; 1<=n ; i++) {
+//     console.log(i);
     
-}
+// }
+
+// 2-masalasi
+
+// let n = +prompt("son kiriting");
+
+// for (let i = n ; i>=1; i--) {
+//     console.log(i);
+    
+// }
+
+// 3-masalasi
+
+// let n = +prompt("son kiriting");
+
+// for (let i =1; 1<=n; i++ ) {
+// if ( (1+n)/2*n ) {
+//     console.log(i);
+// }
+    
+// }
+
+// 4-masalasi
+
+// let n = +prompt("son kiriting");
+
+// for (let i =2; 1<=n; i+=2 ) {
+// if ( (1+n)/2*n && i%2==0 ) {
+//     console.log(i);
+// }
+    
+// }
+
+// 5-masalasi
+
+// let n = +prompt("son kiriting");
+
+// for (let i =1; 1<=n; i+=2 ) {
+// if ( (1+n)/2*n && i%2==1 ) {
+//     console.log(i);
+// }
+    
+// }
+
+// 6-masalasi
+
+// let son = +prompt("Istagan soningizni kiriting (min: 15)")
+
+// for (let i = 1; i <= son; i++) {
+//     let output = "";
+//     if (i % 3 === 0) output += "Fizz";
+//     if (i % 5 === 0) output += "Buzz";
+//     console.log(output || i);
+//   }
+
+// 7-masalasi
+
